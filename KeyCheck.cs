dc900a7981caa8869c8bcc5158291245f37c6db2
VacuouS <vacuous21@gmail.com>
2022-03-23 21:46:02 +0300

    first commit

---
+++ KeyCheck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp
+{
+    class KeyCheck
+    {
+        public event EventHandler<char>? OnKeyPressed;
+
+        public void Run()
+        {
+       
+            char c;
+            while ((c = Console.ReadKey().KeyChar) != 'c')
+            {
+              
+                OnKeyPressed?.Invoke(this, c);
+                
+            }
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static String str = "";
+        public static void Main(string[] args)
+        {
+            var keyHandler = new KeyCheck();
+            keyHandler.OnKeyPressed += ProcessChar;
+            keyHandler.Run();
+            Console.WriteLine("До нажатия 'c' было введено - "+str);
+        }
+
+        private static void ProcessChar(object? sender, char c)
+        {
+            str += c;
+        }
+    }
+}
